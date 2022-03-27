c4068aad547bd6c6bc836c92d919c6668614c80a
gpmas <gpmas117@gmail.com>
2022-03-28 00:00:53 +0100

    Added in notification Manager and calculation for calorie burn. Next Update will add in internal database with some examples to be used in the final version.

--- app/src/main/java/com/example/contextualtrigger/MainActivity.java
+++ app/src/main/java/com/example/contextualtrigger/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.contextualtrigger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -15,7 +20,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor mStepCounter;
     private boolean isCounterSensorPresent;
+    private String textTitle,textContent;
     int stepCount = 0;
+    int CalorieCounnt = 0;
 
 
     @Override
@@ -24,8 +31,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("1","CalorieBurn",NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         textViewStepCounter = findViewById(R.id.textViewStepCounter);
+        textTitle = "You've burned" + CalorieCounnt + " Calories!";
+        textContent = "Thats the same as ";
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
@@ -35,6 +52,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             textViewStepCounter.setText("Counter Sensor Not Found");
             isCounterSensorPresent = false;
         }
+
+        CalorieCounnt = (int) (stepCount * 0.04);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"1")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(1, builder.build());
+
+
+
     }
 
     @Override
@@ -64,4 +95,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
             sensorManager.unregisterListener(this,mStepCounter);
     }
+
 }
